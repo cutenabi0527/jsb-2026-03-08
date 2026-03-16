ae1b8d538ab8944a91e78f57243b37ae74a49bc6
박하민 <lgs7042@naver.com>
2026-03-17 01:33:19 +0900

    jpa dependency

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,20 +2,23 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    // GET httlp://localhost:8080/
+    // GET http://localhost:8080/
     @GetMapping("/")
-    public void home(){
+    @ResponseBody
+    public int home(){
         System.out.println("home");
-        this.contact( );
+        return 22;
     }
-    // GET httlp://localhost:8080/about
+    // GET http://localhost:8080/about
     @GetMapping("/about")
-    public void about(){
+    @ResponseBody
+    public String about(){
         System.out.println("about");
-        this.contact( );
+        return "안녕하세요.";
     }
     public void contact(){
         System.out.println("contact");
